56831b7193790960323bd05998e2801404e690f5
kinnera <kinnera.b2@gmail.com>
2017-08-22 11:10:31 -0400

    1st

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+var questions = {[
+        {q1: "What is the longest river in the United States?",
+        answers: ["COLORADO RIVER", " MISSISSIPPI RIVER", " MISSOURI RIVER ", "RIO GRANDE RIVER"],
+        correctAnswer: "MISSOURI RIVER ",
+        fact: "At 2,565 miles (4,130), the Missouri River is the longest river in the United States and drains approximately one sixth of the North American continent. It serves as a tributary of the Mississippi River. The combined Missouri-Mississippi river system is the fourth longest river in the world.",
+        },
+    {
+        q2: "What state is bordered by Nebraska, Kansas, Oklahoma, New Mexico, Utah, and Wyoming?",
+        answers: ["IDAHO", "MISSOURI", "TEXAS", "COLORADO"],
+        correctAnswer: "COLORADO",
+        fact:"Colorado is bordered by Nebraska, Kansas, Oklahoma, New Mexico, Utah, and Wyoming. Colorado means \"colored red\" and is known as the \"Centennial State\".",
+    },
+    {
+        q3: "Death Valley is located in what U.S. state?",
+        answers: ["NEVADA", "UTAH", "CALIFORNIA", "TEXAS"],
+        correctAnswer:"CALIFORNIA",
+        fact:"Given its name during the 1849 gold rush by emigrants looking to cross it, Death Valley is located in the northern Mojave Desert of southern California. Famous for its brutal heat, on July 10, 1913, it became the location of the hottest temperature ever recorded in the United States (and the second hottest ever recorded in the world) with a scalding temperature of 134°F (56.6°C). The valley usually receives less than 2 inches (5 cm) of rain each year. Ironically, during the late Pleistocene period, Death Valley was submerged beneath a prehistoric lake!",
+    },
+
+}];
+console.log(questions.q1);
